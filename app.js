525ae418b0f6f3d11be12f894420e28874ca17bd
mojocolony <iamwilwal@gmail.com>
2026-09-23 08:53:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import {
   uploadBoard,
 } from './dropbox.js';
 
-const VERSION = '0.1.5';
+const VERSION = '0.1.6';
 const STORAGE_KEY = 'chatboard.board.v1';
 const FONT_KEY = 'chatboard.fontScale.v1';
 const VIEW_KEY = 'chatboard.view.v1';
